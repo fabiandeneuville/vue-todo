fdee84283f17aa4eaff1722e8b523f42c8530cbd
Deneuville Fabian <deneuville.fabian@live.fr>
2022-08-12 16:40:42 +0200

    Initial commit + header

---
+++ src/components/pageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <header class="header">
+        <h1 class="header__title">Vue To Do</h1>
+        <p class="header__subtitle">A basic to do list with Vue</p>
+    </header>
+
+</template>
+
+<script>
+
+export default {
+    name:'pageHeader'
+}
+
+</script>
